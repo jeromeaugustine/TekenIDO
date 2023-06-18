8cebfb4f07c2e2d51a74514ce37abf033de1cbd8
Cosmodude <vladislav.lenskii@gmail.com>
2023-06-18 20:39:22 +0900

    token contract updated

--- contracts/BraqToken.sol
+++ contracts/BraqToken.sol
@@ -4,30 +4,42 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Braq is ERC20, Ownable {
+contract BraqToken is ERC20, Ownable {
 
-    uint256 public publicSaleSupply = 0;
-    // delete this
-    address public ecosystemPool;
-    address public rewardsPool;
-    address public stakingPool;
-    address public claimingPool;
-
+    uint256 public publicSaleSupply = 10;
+    
     mapping(address => bool) public admins;
-    mapping(string => bool) public funded;
-    mapping(string => address) public poolAddress;
-    mapping(string => uint256) public amountToFund;
-    mapping(string => uint256) public fundingTime;
+
+    enum Pools {Rewards, Incentives, Listings, Team, Marketing, Private, Ecosystem}
+    
+    mapping(Pools => mapping(uint8 => bool)) public funded;
+    mapping(Pools => address) public poolAddress;
+    mapping(Pools => mapping(uint8 => uint256)) public amountToFund; // In BraqTokens, multiply by decimals()
+    mapping(Pools => mapping(uint8 => uint256)) public fundingTime;
 
     // Modifier to restrict access to admins only
     modifier onlyAdmin() {
         require(admins[msg.sender], "Only admins can call this function.");
         _;
     }
-    
-    constructor() ERC20("Braq", "BRQ") {
-        _mint(address(this), 150000000 * 10 ** decimals());
+
+    constructor() ERC20 ("Braq", "BRQ") {
+        // minting only public sale tokens
+        _mint(address(this), publicSaleSupply * 10 ** decimals());
         admins[msg.sender] = true;
+        // Setting Pools
+        // Ecosystem
+        for (uint8 i = 0; i < 5; i++) {
+            amountToFund[Pools.Ecosystem][i]=5 * 10 ** 6;
+        }
+        for (uint8 i = 5; i < 9; i++) {
+            amountToFund[Pools.Ecosystem][i]= 2.5 * 10 ** 6;
+        }
+        for (uint8 i = 10; i < 17; i++) {
+            amountToFund[Pools.Ecosystem][i]= 1.25 * 10 ** 6;
+        }
+        // Rewards
+        
     }
 
     // Function to add an admin
@@ -40,38 +52,34 @@ contract Braq is ERC20, Ownable {
         admins[_admin] = false;
     }
     
-    function setPoolAddress(string memory pool, address adr) external onlyAdmin {
-        if(adr==address(0)){
-            revert("Errror: Insert a valid address");
-        }
+    function setPoolAddress(Pools pool, address adr) external onlyAdmin {
+        require(adr != address(0), "Error: Insert a valid address");
         poolAddress[pool]=adr;
     }
 
-    function fundPool(string memory pool) external onlyAdmin {
-        if (block.timestamp < fundingTime[pool]){
+    // Quarters counted from July 2023
+    function fundPool(Pools pool, uint8 quarter) external onlyAdmin {
+        if (block.timestamp < fundingTime[pool][quarter]){
             revert("Error: Too early");
         }
-        if(funded[pool]==true){
-            revert("Errror: Already funded");
-        }
-        if(poolAddress[pool]==address(0)){
-            revert("Errror: Pool not initialised");
-        }
+        require(funded[pool][quarter] == false, "Errror: Already funded");
+        require(poolAddress[pool] != address(0), "Errror: Pool not initialised");
 
-        _transfer(address(this), poolAddress[pool], amountToFund[pool] * 10 ** decimals());
-        funded[pool]= true;
+        _mint(poolAddress[pool], amountToFund[pool][quarter] * 10 ** decimals());
+        funded[pool][quarter]= true;
     }
 
     function publicSale() public payable {
-        if(msg.value< 100 * 10 ** decimals()){
+        uint256 EthAmount = msg.value / 10 ** 18;
+        uint256 BraqAmount = EthAmount * 200;
+        if(EthAmount< 100 ){
             revert("Error: Too small amount for purchase");
         }
-        if(msg.value * 10 ** decimals() > publicSaleSupply){
+        if(BraqAmount > publicSaleSupply){
             revert("Error: Too big amount for purchase");
         }
-        uint256 tokenAmount = msg.value * 10 ** decimals();
-        _transfer(address(this), msg.sender, tokenAmount);
-        publicSaleSupply -= tokenAmount;
+        _transfer(address(this), msg.sender, BraqAmount * 10 ** decimals());
+        publicSaleSupply -= BraqAmount;
     }
 
 
