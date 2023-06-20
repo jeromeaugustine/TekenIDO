1b325c45c3f8dacd2ec6e67bdc9600dcc917f064
Cosmodude <vladislav.lenskii@gmail.com>
2023-06-20 16:30:59 +0900

    updated the contract

--- contracts/BraqToken.sol
+++ contracts/BraqToken.sol
@@ -5,17 +5,26 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract BraqToken is ERC20, Ownable {
-
+    uint256 public allowListSaleSupply = 10; // in braq tokens
     uint256 public publicSaleSupply = 10;
     mapping(address => bool) public admins;
-    
+
     struct Pool {
         mapping(uint8 => bool) funded;
-        mapping(uint8 => uint256) amountToFund;  // In BraqTokens, multiply by decimals()
+        mapping(uint8 => uint256) amountToFund; // In BraqTokens, multiply by decimals()
         address poolAddress;
     }
-    enum Pools {Rewards, Incentives, Listings, Team, Marketing, Private, Ecosystem}
-    mapping (Pools => Pool) public pools;
+
+    enum Pools {
+        Rewards,
+        Incentives,
+        Listings,
+        Team,
+        Marketing,
+        Private,
+        Ecosystem
+    }
+    mapping(Pools => Pool) public pools;
     mapping(uint8 => uint256) fundingTime;
 
     // Modifier to restrict access to admins only
@@ -24,7 +33,7 @@ contract BraqToken is ERC20, Ownable {
         _;
     }
 
-    constructor() ERC20 ("Braq", "BRQ") {
+    constructor() ERC20("Braq", "BRQ") {
         // minting only public sale tokens
         _mint(address(this), publicSaleSupply * 10 ** decimals());
         admins[msg.sender] = true;
@@ -47,20 +56,19 @@ contract BraqToken is ERC20, Ownable {
         fundingTime[14] = 1790780400;
         fundingTime[15] = 1798729200;
         fundingTime[16] = 1806505200;
- 
+
         // Setting Pools
         // Ecosystem
         for (uint8 i = 0; i < 5; i++) {
-            pools[Pools.Ecosystem].amountToFund[i]=5 * 10 ** 6;
+            pools[Pools.Ecosystem].amountToFund[i] = 5 * 10 ** 6;
         }
         for (uint8 i = 5; i < 9; i++) {
-            pools[Pools.Ecosystem].amountToFund[i]= 25 * 10 ** 5;
+            pools[Pools.Ecosystem].amountToFund[i] = 25 * 10 ** 5;
         }
         for (uint8 i = 10; i < 17; i++) {
-            pools[Pools.Ecosystem].amountToFund[i]= 125 * 10 ** 4;
+            pools[Pools.Ecosystem].amountToFund[i] = 125 * 10 ** 4;
         }
         // Rewards
-        
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
@@ -76,40 +84,61 @@ contract BraqToken is ERC20, Ownable {
     function removeAdmin(address _admin) external onlyAdmin {
         admins[_admin] = false;
     }
-    
+
     function setPoolAddress(Pools _pool, address _address) external onlyAdmin {
         require(_address != address(0), "Error: Insert a valid address");
-        pools[_pool].poolAddress=_address;
+        pools[_pool].poolAddress = _address;
     }
 
-    function getPoolAddress(Pools _pool) external view returns (address) { 
+    function getPoolAddress(Pools _pool) external view returns (address) {
         return pools[_pool].poolAddress;
     }
+
+    function getAmountToFund(
+        Pools pool,
+        uint8 quarter
+    ) external view returns (uint256) {
+        return pools[pool].amountToFund[quarter];
+    }
+
     // Quarters counted from July 2023
     function fundPool(Pools _pool, uint8 _quarter) external onlyAdmin {
-        require(_quarter>0 && _quarter<17, "Wrong quarter value");
-        if (block.timestamp < fundingTime[_quarter]){
+        require(_quarter > 0 && _quarter < 17, "Wrong quarter value");
+        if (block.timestamp < fundingTime[_quarter]) {
             revert("Error: Too early");
         }
-        require(pools[_pool].funded[_quarter] == false, "Errror: Already funded");
-        require(pools[_pool].poolAddress != address(0), "Errror: Pool not initialised");
+        require(
+            pools[_pool].funded[_quarter] == false,
+            "Errror: Already funded"
+        );
+        require(
+            pools[_pool].poolAddress != address(0),
+            "Errror: Pool not initialised"
+        );
 
-        _mint(pools[_pool].poolAddress, pools[_pool].amountToFund[_quarter] * 10 ** decimals());
-        pools[_pool].funded[_quarter]= true;
+        _mint(
+            pools[_pool].poolAddress,
+            pools[_pool].amountToFund[_quarter] * 10 ** decimals()
+        );
+        pools[_pool].funded[_quarter] = true;
     }
 
     function publicSale() public payable {
-        uint256 EthAmount = msg.value / 10 ** 18;
-        uint256 BraqAmount = EthAmount * 200;
-        if(EthAmount< 100 ){
+        //uint256 EthAmount = msg.value / 10 ** 18;
+        uint256 BraqAmount = msg.value / 5 * 10 ** 13 ;
+        if (BraqAmount < 1) {
             revert("Error: Too small amount for purchase");
         }
-        if(BraqAmount > publicSaleSupply){
+        if (BraqAmount > publicSaleSupply) {
             revert("Error: Too big amount for purchase");
         }
         _transfer(address(this), msg.sender, BraqAmount * 10 ** decimals());
         publicSaleSupply -= BraqAmount;
     }
 
-
+    function withdraw(uint256 amount) external onlyAdmin {
+        require(address(this).balance >= amount, "Insufficient contract balance");
+        // Transfer ETH to the caller
+        payable(msg.sender).transfer(amount);
+    }
 }
