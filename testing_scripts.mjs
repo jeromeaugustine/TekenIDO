7b36ae4038c5ed95d19562ece51734866dedad66
Cosmodude <vladislav.lenskii@gmail.com>
2023-06-23 06:45:39 +0700

    deployed and tested locally

--- testing_scripts.mjs
+++ testing_scripts.mjs
@@ -7,9 +7,11 @@ import { createInterface } from 'readline';
 const fsPromises = promises;
 loadEnv();
 
+const PUBLIC_KEY = process.env.WALLET_PUBLIC_ADDRESS;
 const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY;
-const TOKEN_CONTRACT_ADDRESS = "0x54d5481C61e2394545EA55CE58706CB1bF260AA7";
-const SALE_CONTRACT_ADDRESS = "0x990166E78Fa1B54145437f5c1720Bc7C3DC8764C";
+const NOTOWNER_PRIVATE_KEY = process.env.NOTOWNER_WALLET_PRIVATE_KEY;
+const TOKEN_CONTRACT_ADDRESS = "0x5757077dC977EEF5c9479d8496C3C9E399b80E88";
+const SALE_CONTRACT_ADDRESS = "0xAC60EF2147a26950EdD76209f7B485C283ff2BA9";
 const my_address = "0x6f9e2777D267FAe69b0C5A24a402D14DA1fBcaA1";
 const Summer_address = "0x19294812D348aa770b006D466571B6D6c4C62365";
 const TOKEN_ABI_FILE_PATH = './build/contracts/BraqToken.json';
@@ -18,7 +20,7 @@ const WHITELIST_CSV_PATH = './whiteList1.csv';
 
 //const provider = ethers.getDefaultProvider(`https://sepolia.infura.io/v3/0cbd49cd77ed4132b497031ffc95da6a`);
 const provider = ethers.getDefaultProvider(`https://sepolia.infura.io/v3/b4ceed0a8862403d802e4bb169d3cb14`);
-const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+const signer = new ethers.Wallet(NOTOWNER_PRIVATE_KEY, provider);
 
 async function getContract(contractAddress, ABIfilePath){
     const data = await fsPromises.readFile(ABIfilePath, 'utf8');
@@ -83,6 +85,11 @@ export async function withdrawETH(_amount){
     return withdrawal;
 }
 
+export async function setToken(tokenAddress){
+    const set = sale_contract.setTokenContract(tokenAddress);
+    return set;
+}
+
 export async function getTokenAddress(){
     return await sale_contract.getTokenAddress();
 }
@@ -92,41 +99,11 @@ export async function addToWhiteList(array){
     return added;
 }
 
-export async function verify() {
-    const withdrawal = await token_contract.verify();
-    return withdrawal;
-}
 // Methods for checking Contracts functionality
-
-/*
-const mint = await mintTokens(my_address);
-const receipt = await provider.waitForTransaction(mint.hash);
-*/
-
-const Pools = {
-    Rewards: 0,
-    Incentives: 1,
-    Listings: 2,
-    Team: 3,
-    Marketing: 4,
-    Private: 5,
-    Ecosystem: 6
-  };
   
 console.log(await totalSupply() / BigInt(10 ** 18));
-/*
-const poolSet = await setPoolAddress(Pools.Ecosystem, "0x19294812D348aa770b006D466571B6D6c4C62365");
-await poolSet.wait();
-console.log("Ecosystem ", await getPoolAddress(Pools.Ecosystem));
-console.log( "Owner ", await getOwner());
-*/
 
-//console.log(await verify()); 
-/*
-const {pool, quarter, tx} = await fundPool(Pools.Ecosystem, 16);
-await tx.wait();
-console.log("Funded ", pool, quarter, "\n tx: ", tx);
-*/
+
 //await startPublicSale();
 //await publicSale();
 //const tokenAddress = await getTokenAddress();
@@ -134,27 +111,24 @@ console.log("Funded ", pool, quarter, "\n tx: ", tx);
 //const mint_tx = await mintTokens("0xC520944Bc9D498b7BeFE887300c501C1D9651B75", 3750000);
 //mint_tx.wait();
 //console.log(mint_tx);
-//const start = await startPublicSale();
+/*
 console.log(await totalSupply() / BigInt(10 ** 18));
 const array = await readAddressesFromCSV(WHITELIST_CSV_PATH);
-await addToWhiteList(array);
-//const mint = await mintTokens(my_address, 500);
-//await mint.wait();
-public 
+console.log(array);
+const WL = await addToWhiteList(array);
+WL.wait();
+console.log(WL);
+const tokenSet = await setToken(TOKEN_CONTRACT_ADDRESS);
+tokenSet.wait();
+const tokenAdr = await getTokenAddress();
+console.log(tokenAdr);
+const mint_tx = await mintTokens(SALE_CONTRACT_ADDRESS, 3750000);
+mint_tx.wait();
+console.log(mint_tx);
 console.log(await totalSupply() / BigInt(10 ** 18));
-
+const start = await startPublicSale();
+*/
+await publicSale();
 //await withdrawETH(BigInt(0.23 * 10 ** 18));
 
 
-// Functions to add new admin
-
-/*
-const newAdmin = await my_contract.addAdmin(Summer_address);
-console.log(newAdmin);
-*/
-
-//await setURI("ipfs://QmbbTwoKVcXazHfxBQgUNtMGqQc5JbTNCSKGBs3GFyWA1D")
-
-// Mint
-//const mint = await mintToken(my_address);
-//console.log(mint);
