1896b94a83b4c18a61c085ed9e14100a32c520f6
Cosmodude <vladislav.lenskii@gmail.com>
2023-06-23 13:49:37 +0700

    chenged migrations

--- migrations/1_deploy_token.js
+++ migrations/1_deploy_token.js
@@ -1,5 +1,8 @@
 const BraqToken = artifacts.require("BraqToken");
+const PublicSale = artifacts.require("BraqPublicSale");
 
-module.exports = function (deployer) {
-  deployer.deploy(BraqToken);
-};
+module.exports = async function (deployer) {
+  deployer.deploy(BraqToken).then(function() {
+    return deployer.deploy(PublicSale, BraqToken.address);
+  });
+}
